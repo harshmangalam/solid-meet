2768a44eaa01b8cf755cce6b6fdc36e2e2ffc8e3
Harsh Mangalam <hmangalam@oplglobal.com>
2022-01-27 16:42:18 +0530

    meeting screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { lazy } from "solid-js";
 
 const MainLayout = lazy(() => import("./screens/main"));
 const Home = lazy(() => import("./screens/main/Home"));
+const Meeting = lazy(() => import("./screens/Meeting"));
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />}></Route>
       </Route>
+      <Route path="/:meetCode" element={<Meeting/>} />
     </Routes>
   );
 }
--- src/components/NewMeeting.jsx
+++ src/components/NewMeeting.jsx
@@ -1,8 +1,20 @@
 import { RiMediaVideoAddFill } from "solid-icons/ri";
 
+import shortid from "shortid";
+import { useNavigate } from "solid-app-router";
+
 export default function NewMeeting() {
+  const navigate = useNavigate();
+
+  const handleMeet = () => {
+    const code = shortid.generate();
+    navigate(`/${code}`);
+  };
   return (
-    <button className="flex items-center gap-3 justify-center bg-blue-500 text-white rounded px-3 py-3 font-bold">
+    <button
+      onClick={handleMeet}
+      className="flex items-center gap-3 justify-center bg-blue-500 text-white rounded px-3 py-3 font-bold"
+    >
       <RiMediaVideoAddFill />
       New meeting
     </button>
