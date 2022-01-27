ea441615f2aca3a96227e4b290b036e56c591bc8
Harsh Mangalam <hmangalam@oplglobal.com>
2022-01-27 08:32:10 +0530

    tailwindcss setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+   <h1 className="text-blue-600">Helloooooooooooooooo</h1>
+  );
+}
+
+export default App;
---
+++ src/screens/home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+     
+    </div>
+  );
+}
